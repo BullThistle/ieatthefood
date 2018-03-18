dd83883d56d670f635f15a9873feb3daea7ca11a
Fals3Andro1d <davilasturias89@gmail.com>
2018-03-18 14:00:40 -0700

    Turned the map into a component so the yelp API integration will be easier. Future access keys will be handled from .env files and can be accessed via process.env like in the new Map component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import React from 'react'
-import { Container, Divider, Dropdown, Grid, Header, Image, List, Menu, Segment } from 'semantic-ui-react'
+import React from 'react';
+import { Container, Divider, Dropdown, Grid, Header, Image, List, Menu, Segment } from 'semantic-ui-react';
 import ReactMapboxGl, { Layer, Feature } from "react-mapbox-gl";
 import Geolocater from './Geolocated.js';
 import 'semantic-ui-css/semantic.css';
 import './App.css';
-
-const Map = ReactMapboxGl({
-  accessToken: ""
-});
+import Map from './components/Map';
 
 const App = (props) => (
   <div>
@@ -29,19 +26,7 @@ const App = (props) => (
     </Container>
     
     <Container text className="centered">
-        <Map
-          style="mapbox://styles/mapbox/streets-v9"
-          containerStyle={{
-            height: "50vh",
-            width: "50vw",
-          }}>
-            <Layer
-              type="symbol"
-              id="marker"
-              layout={{ "icon-image": "marker-15" }}>
-              <Feature coordinates={[-0.481747846041145, 51.3233379650232]}/>
-            </Layer>
-        </Map>
+        <Map />
     </Container>
 
     <Segment
